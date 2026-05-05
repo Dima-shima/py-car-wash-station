bd0e6792ff1c0cb5c523593ba228886e95db4962
Dima-shima <shifrin1970@gmail.com>
2026-05-05 19:49:45 +0300

    All tests passed

--- app/main.py
+++ app/main.py
@@ -17,14 +17,15 @@ class CarWashStation:
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-
     def serve_cars(self, car_list: list) -> float:
         income = 0
         for car in car_list:
             if car.clean_mark < self.clean_power:
                 dif_clean = self.clean_power - car.clean_mark
-                rating_distance = self.average_rating / self.distance_from_city_center
-                income_car = car.comfort_class * dif_clean * rating_distance
+                rating_distance = \
+                    self.average_rating / self.distance_from_city_center
+                income_car = \
+                    car.comfort_class * dif_clean * rating_distance
                 income += income_car
                 # car.clean_mark = self.clean_power
                 self.wash_single_car(car)
@@ -32,15 +33,15 @@ class CarWashStation:
         income = income.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
         return float(income)
 
-
     def calculate_washing_price(self, car: Car) -> float:
         dif_clean = self.clean_power - car.clean_mark
-        rating_distance = self.average_rating / self.distance_from_city_center
+        rating_distance = \
+            self.average_rating / self.distance_from_city_center
         income_car = car.comfort_class * dif_clean * rating_distance
         income_car = Decimal(income_car)
-        income_car = income_car.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
+        income_car = \
+            income_car.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
         return float(income_car)
-
 
     def rate_service(self, rate: int) -> float:
         old_rates = self.average_rating * self.count_of_ratings
@@ -52,7 +53,6 @@ class CarWashStation:
         self.average_rating = new_rates
         self.count_of_ratings = new_count
         return new_rates
-
 
     def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
