5e8e737cfbdbd8d5b708aab585f56581649023cc
Dima-shima <shifrin1970@gmail.com>
2026-05-05 11:24:11 +0300

    Almoust implement

--- app/main.py
+++ app/main.py
@@ -28,19 +28,21 @@ class CarWashStation:
                 income += income_car
                 car.clean_mark = self.clean_power
         income = Decimal(income)
-        return income.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
+        income = income.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
+        return float(income)
 
 
     def calculate_washing_price(self, car: Car) -> float:
         dif_clean = self.clean_power - car.clean_mark
         rating_distance = self.average_rating / self.distance_from_city_center
         income_car = car.comfort_class * dif_clean * rating_distance
-        return round(income_car, 1)
+        income_car = Decimal(income_car)
+        income_car = income_car.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
+        return float(income_car)
 
 
     def wash_single_car(self, car: Car) -> None:
-        if car.clean_mark < self.clean_power:
-            car.clean_mark = self.clean_power
+        car.clean_mark = self.clean_power
 
 
     def rate_service(self, rate: int) -> float:
@@ -48,6 +50,8 @@ class CarWashStation:
         new_count = self.count_of_ratings + 1
         new_rates = (old_rates + rate) / new_count
         new_rates = Decimal(new_rates)
-        self.average_rating = new_rates.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
+        new_rates = new_rates.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
+        new_rates = float(new_rates)
+        self.average_rating = new_rates
         self.count_of_ratings = new_count
-        return new_rates.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
+        return new_rates
