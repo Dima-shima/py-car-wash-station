4f89910d338a6af20ffff7333263844b66c32dd3
Dima-shima <shifrin1970@gmail.com>
2026-05-05 03:11:03 +0300

    Almoust implement

--- app/main.py
+++ app/main.py
@@ -2,55 +2,52 @@ from decimal import Decimal, ROUND_HALF_UP
 
 
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand: str):
+    def __init__(self, comfort_class: int,
+                 clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center, clean_power, average_rating, count_of_ratings):
+    def __init__(self, distance_from_city_center: int, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
 
-    def serve_cars(self, car_list):
-        income = 0
-        for car in car_list:
-            if car.clean_mark < self.clean_power:
-                dif_clean = self.clean_power - car.clean_mark
-                rating_distance = self.average_rating / self.distance_from_city_center
-                income_car = car.comfort_class * dif_clean * rating_distance
-                income += income_car
-                car.clean_mark = self.clean_power
-        income = Decimal(income)
-        return income.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
-        # return round(income, 1)
-
-
-    def calculate_washing_price(self, car):
-        dif_clean = self.clean_power - car.clean_mark
-        rating_distance = self.average_rating / self.distance_from_city_center
-        income_car = car.comfort_class * dif_clean * rating_distance
-        # income_car = Decimal(income_car)
-        # return income_car.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
-        return round(income_car, 1)
-
-
-    def wash_single_car(self, car):
+def serve_cars(self, car_list: list) -> float:
+    income = 0
+    for car in car_list:
         if car.clean_mark < self.clean_power:
+            dif_clean = self.clean_power - car.clean_mark
+            rating_distance = self.average_rating / self.distance_from_city_center
+            income_car = car.comfort_class * dif_clean * rating_distance
+            income += income_car
             car.clean_mark = self.clean_power
+    income = Decimal(income)
+    return income.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
 
 
-    def rate_service(self, rate):
-        old_rates = self.average_rating * self.count_of_ratings
-        new_count = self.count_of_ratings + 1
-        new_rates = (old_rates + rate) / new_count
-        new_rates = Decimal(new_rates)
-        self.average_rating = new_rates.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
-        self.count_of_ratings = new_count
-        return new_rates.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
-        # return round(new_rates, 1)
+def calculate_washing_price(self, car: Car) -> float:
+    dif_clean = self.clean_power - car.clean_mark
+    rating_distance = self.average_rating / self.distance_from_city_center
+    income_car = car.comfort_class * dif_clean * rating_distance
+    return round(income_car, 1)
 
+
+def wash_single_car(self, car: Car) -> None:
+    if car.clean_mark < self.clean_power:
+        car.clean_mark = self.clean_power
+
+
+def rate_service(self, rate: int) -> float:
+    old_rates = self.average_rating * self.count_of_ratings
+    new_count = self.count_of_ratings + 1
+    new_rates = (old_rates + rate) / new_count
+    new_rates = Decimal(new_rates)
+    self.average_rating = new_rates.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
+    self.count_of_ratings = new_count
+    return new_rates.quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
